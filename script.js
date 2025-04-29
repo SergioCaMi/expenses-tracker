f06fce36412b3117ceda66bebb710c0b3218b128
SergioCaMi <sergiocami84@gmail.com>
2025-04-29 20:42:53 +0200

    done

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
 // Happy coding
 
 // Primeros pasos: carga el fichero JSON y muestra su contenido en la consola. Luego, ya puedes implementar la iteración 1. Para mostrar la fecha legible, puedes buscar por Chat GPT o por Google como convertir un timestamp 
+
+const fs = require("fs");
+const path = require("path");
+const directory = "./";
+const filePath = path.join(directory, "expenses.json");
+const arguments = process.argv.slice(2);
+
+fs.readFile(filePath, "utf-8", (err) => {
+    if (err) {
+      console.log("Error al leer el directorio", err);
+    } else {
+        if (arguments.at(-1)== "--list"){
+            console.log("listado");
+        }
+    }
+  });
